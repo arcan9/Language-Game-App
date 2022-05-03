143e71674139e7a6fac7bec9fa8792c3d3f88fa5
ravencancio <102492480+arcan9@users.noreply.github.com>
2022-05-02 19:34:19 -0700

    set up question as Object

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ let question = {
   alternatives: ["dog", "cat", "bird", "fish"],
   correctAnswer: 1,
 };
+
+function showQuestion(q) {
+  let titleDiv = document.getElementById("title");
+  titleDiv.textContent = q.title;
+
+  let alts = document.querySelectorAll(".alternative");
+
+  alts.forEach(function (element, index) {
+    element.textContent = q.alternatives[index];
+  });
+}
+
+showQuestion(question);
