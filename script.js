717b33f61b9e00ea0423b963cc41ade811cbc8b5
ravencancio <102492480+arcan9@users.noreply.github.com>
2022-05-03 21:13:54 -0700

    Modify code flow
    Moved event listener in a separate function. Initialized only once when app starts.

--- script.js
+++ script.js
@@ -4,33 +4,35 @@ let question = {
   correctAnswer: 1,
 };
 
+function start() {
+  // get the alternatives
+  let alts = document.querySelectorAll(".alternative");
+
+  // event listeners should be initialized at the start of the app
+  // and only happen within this function
+  alts.forEach(function (element, index) {
+    element.addEventListener("click", function () {
+      // log as placeholder
+      console.log("check correct answer");
+    });
+  });
+  // show first question
+  showQuestion(question);
+}
+
+// showQuestion simply shows the question and alternatives
 function showQuestion(q) {
   // select dom element
   // modify dom
   let titleDiv = document.getElementById("title");
   titleDiv.textContent = q.title;
 
-  // select by query
   let alts = document.querySelectorAll(".alternative");
 
   alts.forEach(function (element, index) {
     element.textContent = q.alternatives[index];
-
-    element.addEventListener("click", function () {
-      if (q.correctAnswer == index) {
-        alert("Correct answer!");
-      } else {
-        alert("Wrong answer!");
-      }
-    });
   });
 }
 
-showQuestion(question);
-
-function display() {
-  alert("You clicked me!");
-}
-
-let button = document.getElementById("btn");
-button.addEventListener("click", display);
+// call the start function
+start();
