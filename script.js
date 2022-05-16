e82db278c35d6c592311cb5c5feecc621206ee9c
ravencancio <102492480+arcan9@users.noreply.github.com>
2022-05-16 00:05:22 -0700

    Create questions array and add method calls
    Method calls for subsequent questions

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-let question = {
-  title: "고양이",
-  alternatives: ["dog", "cat", "bird", "fish"],
-  correctAnswer: 1,
-};
+// let question = {
+//   title: "고양이",
+//   alternatives: ["dog", "cat", "bird", "fish"],
+//   correctAnswer: 1,
+// };
+
+let questions = [
+  {
+    title: "고양이",
+    alternatives: ["dog", "cat", "bird", "fish"],
+    correctAnswer: 1,
+  },
+  {
+    title: "집",
+    alternatives: ["book", "school", "house", "playground"],
+    correctAnswer: 2,
+  },
+  {
+    title: "음악",
+    alternatives: ["music", "desk", "song", "food"],
+    correctAnswer: 0,
+  },
+  {
+    title: "강아지",
+    alternatives: ["shoes", "tiger", "horse", "puppy"],
+    correctAnswer: 3,
+  },
+  {
+    title: "코끼리",
+    alternatives: ["nose", "elephant", "rhino", "big"],
+    correctAnswer: 1,
+  },
+];
 
 let app = {
   start: function () {
+    this.currentPosition = 0;
+
     let alts = document.querySelectorAll(".alternative"); // get the alternatives
 
     alts.forEach(
@@ -19,12 +49,10 @@ let app = {
       }.bind(this)
     );
 
-    this.showQuestion(question); // show first question
+    this.showQuestion(questions[this.currentPosition]); // show first question
   },
-  //////
-  showQuestion: function (q) {
-    this.currentQuestion = q; // the question currently shown is stored in this variable
 
+  showQuestion: function (q) {
     let titleDiv = document.getElementById("title");
     titleDiv.textContent = q.title;
 
@@ -34,12 +62,24 @@ let app = {
       element.textContent = q.alternatives[index];
     });
   },
-  //////
+
   checkAnswer: function (userSelected) {
-    if (this.currentQuestion.correctAnswer == userSelected) {
+    currentQuestion = questions[this.currentPosition];
+
+    if (currentQuestion.correctAnswer == userSelected) {
       console.log("correct");
+      this.increasePosition();
+      this.showQuestion(questions[this.currentPosition]);
     } else {
       console.log("wrong");
+    }
+  },
+
+  increasePosition: function () {
+    this.currentPosition++;
+
+    if (this.currentPosition == questions.length) {
+      this.currentPosition = 0;
     }
   },
 };
